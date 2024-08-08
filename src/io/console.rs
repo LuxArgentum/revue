9ab9c4e8ddc7811645aec3bba430bf18809042ad
MatthewSong <matthewsong18@gmail.com>
2024-08-07 18:52:15 -0700

    Add console

---
+++ src/io/console.rs
@@ -0,0 +1,13 @@
+fn start_console() {
+    println!("|----- SIR-Tracker -----|");
+
+    display_todays_review_topics();
+}
+
+fn display_todays_review_topics() {
+    println!("Today's Review Topics");
+
+    get_todays_topics();
+}
+
+fn get_todays_topics() {}
