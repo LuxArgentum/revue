4a54d5fabc22a3863034984e72ff17dc415d7e99
MatthewSong <matthewsong18@gmail.com>
2024-08-08 21:02:01 -0700

    Add console display for today and all topics

--- src/io/console.rs
+++ src/io/console.rs
@@ -1,13 +1,40 @@
-fn start_console() {
+use crate::io::storage::Storage;
+use crate::topics::review_topics::ReviewTopic;
+
+pub fn start_console() {
     println!("|----- SIR-Tracker -----|");
 
-    display_todays_review_topics();
+    let mut storage: Storage = Storage::new();
+
+    display_todays_review_topics(&storage);
+
+    display_all_topics(&storage);
+
+    storage.write_storage();
 }
 
-fn display_todays_review_topics() {
-    println!("Today's Review Topics");
-
-    get_todays_topics();
+fn display_all_topics(storage: &Storage) {
+    println!("\nAll Review Topics:");
+    let list = storage.get_review_topic_list();
+    list.iter()
+        .for_each(|topic| println!("{}", topic.topic_name));
 }
 
-fn get_todays_topics() {}
+fn display_todays_review_topics(storage: &Storage) {
+    println!("\nToday's Review Topics:");
+
+    let review_topic_list: Vec<ReviewTopic> = storage.get_review_topic_list();
+    let mut review_topic_not_found: bool = true;
+    review_topic_list.iter().for_each(|topic| {
+        if topic.is_time_to_review() {
+            if review_topic_not_found {
+                review_topic_not_found = false
+            }
+            println!("{}", topic.topic_name)
+        }
+    });
+
+    if review_topic_not_found {
+        println!("No review topics for today")
+    }
+}
