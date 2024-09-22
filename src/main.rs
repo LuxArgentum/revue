cbdd89f668333caba91bc1c4b1bd70609b766c9d
MatthewSong <matthewsong18@gmail.com>
2024-09-21 17:26:13 -0700

    Add review_gap to views
    Signed-off-by: MatthewSong <matthewsong18@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ use clap::Parser;
 use std::io::stdout;
 
 mod io;
-mod scheduling;
 mod topics;
 
 fn main() {
+    env_logger::init();
+
     let cli = Cli::parse();
 
     let mut storage = Storage::new(PreviousStorage::Yes, None);
 
     match &cli.command {
+        // Edit command allows changing the name of a topic to a new name
         Commands::Edit(edit_args) => {
             match storage.rename_review_topic(
                 edit_args.topic_name.to_string(),
@@ -25,15 +27,21 @@ fn main() {
                 }
             }
         }
+
+        // View command allows viewing today's topics or all of the topics
         Commands::View { view } => match view {
             CliState::Today => display_today(&storage, stdout()),
             CliState::All => display_all(&storage, stdout()),
         },
+
+        // Add command allows adding new topics to the storage
         Commands::Add { topic_name } => {
             let new_review_topic = ReviewTopic::new(topic_name.to_string());
             storage.add_review_topic(new_review_topic);
             storage.write_storage();
         }
+
+        // Remove command allows removing topics from the storage
         Commands::Remove { topic_name } => {
             match storage.clone().find_review_topic(topic_name.to_string()) {
                 None => {}
@@ -43,18 +51,10 @@ fn main() {
                 }
             }
         }
-        Commands::Review { topic_name } => {
-            let mut review_list = storage.get_review_topic_list();
-            let topic_index = review_list.iter().position(|topic| topic.topic_name == *topic_name);
 
-            match topic_index {
-                None => { println!("Review topic was not found. Did you misspell?") }
-                Some(topic_index) => {
-                    let mut review_topic = review_list.remove(topic_index);
-                    review_topic.review();
-                    review_list.push(review_topic);
-                }
-            }
+        // Review command marks the provided topic as reviewed and recalculates when to review it next
+        Commands::Review { topic_name } => {
+            storage.review_topic(topic_name.to_owned());
         }
     }
 }
